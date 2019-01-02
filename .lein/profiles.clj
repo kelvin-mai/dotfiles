309c156fbfda419b80ca362a021a0388fa39f5a4
Kelvin Mai <kelvin.mai002@gmail.com>
2019-01-02 17:30:35 -0500

    Initial commit

---
+++ .lein/profiles.clj
@@ -0,0 +1,3 @@
+{:repl {:plugins [[org.clojure/tools.nrepl "0.2.13"]
+                  [cider/cider-nrepl "0.18.0"]
+                  [venantius/ultra "0.5.2"]]}}
---
+++ README.md
@@ -0,0 +1,3 @@
+# dotfiles
+
+config files
