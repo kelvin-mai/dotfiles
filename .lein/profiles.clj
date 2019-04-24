9b359725c8b61db1fe8e7a98fe9e488ade8d0505
Kelvin Mai <kelvin.mai002@gmail.com>
2019-04-24 14:33:36 -0400

    Update profiles.clj

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,3 +1,1 @@
-{:repl {:plugins [[org.clojure/tools.nrepl "0.2.13"]
-                  [cider/cider-nrepl "0.18.0"]
-                  [venantius/ultra "0.5.2"]]}}
+{:repl {:plugins [[cider/cider-nrepl "0.18.0"]]}}
