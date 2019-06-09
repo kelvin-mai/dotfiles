8d73f0acf271f7e90edbda24f87bbcb10b95001d
Kelvin Mai <kelvin.mai002@gmail.com>
2019-06-08 20:44:19 -0400

    Updated configs

--- linux-alias.sh
+++ linux-alias.sh
@@ -1,6 +1,6 @@
 alias ls='ls -a'
 alias src='source ~/.zshrc'
-alias .zsh='code ~/.zshrc'
+alias .zsh='vim ~/.zshrc'
 alias project='cd /home/kelvin/Projects'
 alias cdp='project'
 alias tutorial='cd /home/kelvin/Tutorials'
