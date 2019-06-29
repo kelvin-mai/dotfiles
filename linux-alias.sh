49d57e2907f20f90e5df3f0cc342e246e0f8bbc2
Kelvin Mai <kelvin.mai002@gmail.com>
2019-06-29 09:48:04 -0400

    Edited mac settings

--- linux-alias.sh
+++ linux-alias.sh
@@ -1,8 +1,5 @@
-alias ls='ls -a'
 alias src='source ~/.zshrc'
 alias .zsh='vim ~/.zshrc'
-alias project='cd /home/kelvin/Projects'
-alias cdp='project'
-alias tutorial='cd /home/kelvin/Tutorials'
-alias cdt='tutorial'
+alias cdp='cd /home/kelvin/Projects'
+alias cdt='cd /home/kelvin/Tutorials'
 
--- mac-alias.sh
+++ mac-alias.sh
@@ -1,13 +1,9 @@
-alias chrome='open -a "google chrome"'
 alias code='open -a "Visual Studio Code"'
 
-alias ls='ls -a'
 alias src='source ~/.zshrc'
 alias .zsh='vim ~/.zshrc'
-alias project='cd /Users/kelvinmai/Projects'
-alias cdp='project'
-alias tutorial='cd /Users/kelvinmai/Tutorials'
-alias cdt='tutorial'
-alias goproject='cd /Users/kelvinmai/go/src/github.com/kelvin-mai/'
+alias cdp='cd /Users/kelvinmai/Projects'
+alias cdt='cd /Users/kelvinmai/Tutorials'
+alias cdgo='cd /Users/kelvinmai/go/src/github.com/kelvin-mai/'
 
 alias mongkill='mongo admin --eval "db.shutdownServer()"'
