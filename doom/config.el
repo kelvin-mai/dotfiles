4afa3ae4561b22929cf2a07020c9883f87de63f2
Kelvin Mai <kelvin.mai002@gmail.com>
2020-05-20 17:18:07 -0700

    added doom configs

--- doom/config.el
+++ doom/config.el
@@ -1,33 +1,6 @@
 (setq user-full-name "Kelvin Mai"
       user-mail-address "kelvin.mai002@gmail.com")
 
-;; Here are some additional functions/macros that could help you configure Doom:
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', where Emacs
-;;   looks when you load packages with `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c g k').
-;; This will open documentation for it, including demos of how they are used.
-;;
-;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
-;; they are implemented.
-(load! "+ui")
+(setq projectile-project-search-path '("~/dev/projects/" "~/dev/tutorials/"))
 
-(add-to-list 'auto-mode-alist '("\\.tsx\\'" . rjsx-mode))
-(defun setup-tide-mode ()
-  (interactive)
-  (tide-setup)
-  (flycheck-mode +1)
-  (eldoc-mode +1)
-  (tide-hl-identifier)
-  (company-mode +1))
-(setq company-tooltip-align-annotations t)
-(setq tide-completion-ignore-case t)
-(add-hook 'js2-mode-hook #'setup-tide-mode)
-(add-hook 'rjsx-mode-hook #'setup-tide-mode)
-(setq tide-format-options '(:indentSize 2 :tabSize 2))
+(load! "+ui")
--- update.sh
+++ update.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
+
 # change directories if necessary
-export I3_DIR="$HOME/.i3"
-export POLYBAR_DIR="$HOME/.config/polybar"
-export ALACRITTY_DIR="$HOME/.config/alacritty"
+export CONFIG_TARGET="$HOME/.config"
 
 echo "git update submodules"
 git submodule update --init --recursive
@@ -12,24 +11,12 @@ cp .profile $HOME/.profile
 cp .gitconfig $HOME/.getconfig
 cp .zshrc $HOME/.zshrc
 
-echo "updating vim"
-cp -r .vim/ $HOME/
-cp -r .vimrc $HOME/.vimrc
-cp -r .config/nvim $HOME/.config
+echo "updating user interface"
+cp -TRv i3 $CONFIG_TARGET/i3
+cp -TRv polybar $CONFIG_TARGET/polybar
+cp -TRv alacritty $CONFIG_TARGET/alacritty
+cp picom.conf $CONFIG_TARGET/picom.conf
 
-echo "updating tmux"
-cp .tmux.conf $HOME/.tmux.conf
-
-echo "updating $I3_DIR"
-cp .config/i3/config $I3_DIR/config
-cp .config/i3/polybar.sh $I3_DIR/polybar.sh 
-cp .config/i3/i3pwd.sh $I3_DIR/i3pwd.sh
-
-echo "updating $POLYBAR_DIR"
-cp .config/polybar/config $POLYBAR_DIR/config
-
-echo "updating $ALACRITTY_DIR"
-cp .config/alacritty/alacritty.yml $ALACRITTY_DIR/alacritty.yml
-
-echo "updating $HOME/.config/picom.conf"
-cp .config/picom.conf $HOME/.config/picom.conf
+echo "updating editors"
+cp -TRv nvim $CONFIG_TARGET/nvim
+cp -TRv doom $CONFIG_TARGET/doom
