25193e39cd92f95a4e59da6ddacab6da2fceecf6
Kelvin Mai <kelvin.mai002@gmail.com>
2021-03-28 10:26:44 -0700

    initial pop os bspwm config

--- zsh/alias.sh
+++ zsh/alias.sh
@@ -2,10 +2,14 @@
 alias src='source $HOME/.zshrc'
 alias .zsh='vim $HOME/.zshrc'
 
+# Store current directory
+precmd() { eval "pwd > /tmp/whereami" }
+
 # enhancements
 alias ls='lsd -hA --group-dirs first'
 alias vim='nvim'
 alias grep='grep --color=auto'
+alias ranger='source ranger'
 
 # shortcuts
 alias cdp='cd $HOME/Development/projects'
